488b0ff9af682a32b9506db00c4bcb006c12b043
Bojan Milevski <bojan@bojanmilevski.com>
2024-08-18 22:17:40 +0200

    movies are generated before music. fix formatting

--- script.js
+++ script.js
@@ -41,46 +41,46 @@ class Video {
 }
 
 const getEmailLink = () => {
-	const username = 'bojan';
-	const domain = 'bojanmilevski';
-	const tld = 'com';
-	const email = username + '@' + domain + '.' + tld;
-	return 'mailto:' + email;
-}
+  const username = "bojan";
+  const domain = "bojanmilevski";
+  const tld = "com";
+  const email = username + "@" + domain + "." + tld;
+  return "mailto:" + email;
+};
 
 const generate = (div, array) => {
-	const root = document.getElementById(div);
+  const root = document.getElementById(div);
 
-	array.forEach((element) => {
-		element.generate(root);
-	});
+  array.forEach((element) => {
+    element.generate(root);
+  });
 };
 
 generate("icons", [
-	new Icon(getEmailLink(), "cod-mail"),
-	new Icon("https://github.com/bojanmilevski", "md-github"),
-	new Icon("https://linkedin.com/in/bojan-milevski-b81842283", "md-linkedin"),
-	new Icon("https://facebook.com/bojanmilevskii", "fa-facebook_square"),
-	new Icon("https://instagram.com/bojanmilevskii", "md-instagram"),
-	new Icon("https://x.com/bojanmilevskii", "fa-twitter_square"),
-	new Icon("https://reddit.com/user/bojanmilevskii", "md-reddit"),
-	new Icon("https://discord.com/users/1085313022999928892", "fa-discord"),
-	new Icon("https://youtube.com/@bojanmilevski", "md-youtube"),
-	new Icon("https://odysee.com/@bojanmilevski", "fa-user_astronaut"),
-	new Icon("https://open.spotify.com/user/31uf7k245w4wmow4thq7z3dvzdaq?si=797e99aca7d74f82", "md-spotify"),
-]);
-
-generate("music", [
-	new Video("HpAIDSSc_Y0"),
-	new Video("VFle9mIRhFs"),
-	new Video("RQWLTqlTmrE"),
-	new Video("kQoKO_v93g0"),
+  new Icon(getEmailLink(), "cod-mail"),
+  new Icon("https://github.com/bojanmilevski", "md-github"),
+  new Icon("https://linkedin.com/in/bojan-milevski-b81842283", "md-linkedin"),
+  new Icon("https://facebook.com/bojanmilevskii", "fa-facebook_square"),
+  new Icon("https://instagram.com/bojanmilevskii", "md-instagram"),
+  new Icon("https://x.com/bojanmilevskii", "fa-twitter_square"),
+  new Icon("https://reddit.com/user/bojanmilevskii", "md-reddit"),
+  new Icon("https://discord.com/users/1085313022999928892", "fa-discord"),
+  new Icon("https://youtube.com/@bojanmilevski", "md-youtube"),
+  new Icon("https://odysee.com/@bojanmilevski", "fa-user_astronaut"),
+  new Icon("https://open.spotify.com/user/31uf7k245w4wmow4thq7z3dvzdaq?si=797e99aca7d74f82", "md-spotify"),
 ]);
 
 generate("movies", [
-	new Video("6MUcuqbGTxc"),
-	new Video("07-QBnEkgXU"),
-	new Video("r3dcnV6Z9Zs"),
-	new Video("Z06d-ftqO2Y"),
-	new Video("abUgHWAaBFU"),
+  new Video("6MUcuqbGTxc"),
+  new Video("07-QBnEkgXU"),
+  new Video("r3dcnV6Z9Zs"),
+  new Video("Z06d-ftqO2Y"),
+  new Video("abUgHWAaBFU"),
+]);
+
+generate("music", [
+  new Video("HpAIDSSc_Y0"),
+  new Video("VFle9mIRhFs"),
+  new Video("RQWLTqlTmrE"),
+  new Video("kQoKO_v93g0"),
 ]);
